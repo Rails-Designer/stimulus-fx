9fb6dd1e831ac5c16d19c0a7d9592fb7d7a1adad
eelco <git@eelcoj.com>
2024-12-13 19:42:36 +0100

    Add Stimulus debug option via url param

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import * as actions from "./action-options"
-
-export { actions };
+import * as actions from "./action-options";
 
 export function registerActionOptions(application) {
   Object.entries(actions).forEach(([name, method]) => {
     application.registerActionOption(name, method);
   });
 }
+
+export function registerDevMode(application) {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const debug = new URLSearchParams(location.search).get('debug');
+  if (debug === 'true') localStorage.setItem('debug', 'true');
+  if (debug === 'false') localStorage.removeItem('debug');
+
+  application.debug = localStorage.getItem('debug') === 'true';
+}
+
+export { actions };
